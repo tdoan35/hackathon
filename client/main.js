b318b4ff416400d89126776b2b4aca97b554c6b7
Ty Thanh Doan <tdoan35@gmail.com>
2021-03-11 12:29:54 -0800

    Refactored functionality to handle asynch GET calls

--- client/main.js
+++ client/main.js
@@ -1,40 +1,132 @@
 
 
 // get a list of user defined company names (from their portfolio selection)
-let listsofUserSelectedCompanies = ['TSLA', 'PLTR', 'AMZN', 'FB', 'GOOG'];
+// get the company name from stock ticker
 
-// get the stock ticker from the company list from 
 
-$(document).ready(function(){
-  $("p").hide();
-});
 
-// search for all variations of the company name in the JSON object 
+// clear console on every run
+// console.clear()
 
-// const settings = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?q=tesla&region=US",
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-key": "a051609f16msh308e4bc586d8c7dp1560d1jsn3721171120df",
-// 		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
-// 	}
+// get the current page JSON object
+// $(document).ready(function(){
+//   // let stonk = getStockData(companies[0]);
+//   // console.log(stonk['TSLA']);
+
+//   // addTickerPrice(companyNames[0], "668.0600");
+
+//   console.log(getStonkData())
+
+// });
+
+
+// adds the ticker price to the DOM element 
+// function addTickerPrice(company, tickerPriceStr) {
+
+//   //const targetElements = document.querySelectorAll("span, p, h1, h2, h3, a");
+//   // check for doc
+//   const targetElements = document.querySelectorAll("span");
+  
+
+//   // iterate through each target element to look for matching company names
+//    targetElements.forEach( (e) => {
+//     // parse through the text content to see if a matching company name exists
+//     // if a match is found, insert the new ticker price text
+//     if (e.textContent.includes(company)) e.textContent = `${company} - ${tickerPriceStr}`;        
+//   });
 // };
-console.log('Comment outside AJAX call')
-$.ajax({
-  type: 'GET',
-  url: 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=TSLA&interval=5min&apikey=YZZMTHCW6NPA55RJ',
-  success: function(resp) {
-    console.log('SUCCESS WOOT')
 
-  },
-  error: function() {
 
-  }
-});
+ // Given: let companies = ['TSLA', 'PLTR', 'AMZN', 'FB', 'GOOG'];
+ // Output: stonkPrices = {'TSLA': '625', 'PLTR': '23', 'AMZN': '5135', 'FB': '234', 'GOOG': '2003'}
 
-// insert our JSON piece 
+// Given: 'TSLA'
+// Output: '668.000'
 
-// return JSON piece
+// function getStockData(ticker) {
+//   // Initialize a resultant stonks object
+//   let stock = {};
 
+//   $.ajax({
+//     type: 'GET',
+//     url: 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=YZZMTHCW6NPA55RJ',
+//     success: function(resp) {
+  
+//     // let date = new Date();
+//     // let todaysDate = (date.getFullYear()+'-' + '0' + (date.getMonth()+1) + '-'+(date.getDate())).toString();
+//     //stonks[companiesArray[i]] = resp["Time Series (Daily)"][todaysDate]["4. close"];
+//     stock[ticker] = resp["Time Series (Daily)"]["2021-03-10"]["4. close"];
+//     // console.log('successful API call');
+//     // console.log(price);
+//     },
+//     error: function() {
+      
+//     }
+//   });
+  
+//   return stock;
+// }
+ 
+// console.log(getStockData());
+
+// let respObj = {}
+// $.ajax({
+//   type: 'GET',
+//   url: 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=YZZMTHCW6NPA55RJ',
+//   success: function(resp) {
+
+//   // let date = new Date();
+//   // let todaysDate = (date.getFullYear()+'-' + '0' + (date.getMonth()+1) + '-'+(date.getDate())).toString();
+//   //stonks[companiesArray[i]] = resp["Time Series (Daily)"][todaysDate]["4. close"];
+//   respObj = resp;
+//   // console.log('successful API call');
+//   // console.log(price);
+//   },
+//   error: function() {
+    
+//   }
+// });
+
+let companyNames = ['Tesla', 'Palantir', 'Amazon', 'Facebook', 'Google'];
+let companies = ['TSLA', 'PLTR', 'AMZN', 'FB', 'GOOG'];
+
+
+// Given: 'TSLA'
+// Output: '668.000'
+async function getStonkData(ticker) {
+  let resp = await fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+ticker+'&apikey=YZZMTHCW6NPA55RJ')
+  let price = await resp.json();
+  return price["Time Series (Daily)"]["2021-03-10"]["4. close"];
+}
+
+async function main() {
+  const tslaPrice = await getStonkData(companies[0]);
+  addTickerPrice('Tesla', tslaPrice)
+}
+
+// adds the ticker price to the DOM element 
+function addTickerPrice(company, tickerPriceStr) {
+
+  const targetElements = document.querySelectorAll("span, p, h1, h2, h3, a");
+  //const targetElements = document.querySelectorAll("span");
+  
+  // iterate through each target element to look for matching company names
+   targetElements.forEach( (e) => {
+    // parse through the text content to see if a matching company name exists
+    // if a match is found, insert the new ticker price text
+    if (e.textContent.includes(company)) e.textContent = `${company} - $${Number(tickerPriceStr)}`;        
+  });
+};
+
+
+
+// On document ready
+$(document).ready(function() {
+
+  // ------------ Prepend the body document with a div for ticker mover ------------
+
+
+
+  main();
+
+}); // End of document.ready
